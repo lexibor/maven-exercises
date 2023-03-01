79d73404928eff4a153c0bda2efe418b6a635320
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-03-01 13:41:27 -0600

    finished first maven exercise

---
+++ src/main/java/GuavaTest.java
@@ -0,0 +1,33 @@
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class GuavaTest
+{
+    public static void main(String[] args)
+    {
+        String output = Strings.padEnd("cat", 10, '*');
+        System.out.println(output);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+
+        System.out.printf("You entered: \"%s\"%n", userInput);
+
+        if(!StringUtils.isNumeric(userInput))
+        {
+            System.out.printf("\"%s\" is not a number%n", userInput);
+        }
+
+        System.out.printf("Flipped case: %s%n", StringUtils.swapCase(userInput));
+
+        System.out.printf("Reversed: %s", StringUtils.reverse(userInput));
+
+
+
+
+    }
+}
